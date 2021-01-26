f20c032e0c8ace83cf8f49eb50c1be7e4ac37321
German <gnrg_74@hotmail.com>
2021-01-26 12:31:22 -0600

    utilizacion del body parser

--- server.js
+++ server.js
@@ -1,15 +1,25 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+
 const router = express.Router();
 
 var app = express();
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(router);
 
 //Router mini apps
 router.get('/message', function(req,res){
+    console.log(req.headers);
+    res.header({
+        "custom-header": "Valor personalizado",
+    });
     res.send('Lista de mensajes');
 });
 router.post('/message', function(req,res){
-    res.send('Mensaje añadido');
+    console.log(req.query);
+    console.log(req.body);
+    res.send('Mensaje '+ req.body.text +' añadido' );
 });
 // app.use('/', function(req, res){
 //     res.send('hola'); //Responde con un hola en el navegador
