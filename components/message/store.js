2660e7e532d1b0bc18c2463fb05eb786744993fe
German <gnrg_74@hotmail.com>
2021-01-29 12:21:12 -0600

    se agrego bd pen store

--- components/message/store.js
+++ components/message/store.js
@@ -1,4 +1,11 @@
-const list = [];
+const db = require("mongoose")
+const Model = require('./model')
+
+db.Promise = global.Promise //Devuelva las promesas
+db.connect('mongodb+srv://root:<root>@cluster0.gx4fd.mongodb.net/<MessageGo>?retryWrites=true&w=majority',{
+    useNewUrlParser: true, //No haya problema de compatibilidad
+    useUnifiedTopology: true //
+})
 
 function addMessage(message){
     list.push(message);
