da39176e6c718183e3421a8d941ad76c0c9d5966
German <gnrg_74@hotmail.com>
2021-01-29 12:20:48 -0600

    se creo modelo para mongodb

---
+++ components/message/model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+//A traves de un obejto definir todas las propiedades que queremos tenr 
+const mySchema = new Schema({
+    user: String,
+    //Se creo message tipo objeto para mandar un mensaje estrcito y no se guarde ningun tipo de info
+    //que no sea mensjae
+    message: {
+        type: String,
+        required: true,
+    },
+    date: Date
+});
+
+const model = mongoose.model('Message', mySchema); //Collecion en mongo y el esquema
+module.exports = model;
