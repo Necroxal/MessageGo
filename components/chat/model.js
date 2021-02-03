29ccee24a61461158c403318c047ade2b18eb194
German <gnrg_74@hotmail.com>
2021-02-03 13:05:58 -0600

    creacion de un nuevo componente

---
+++ components/chat/model.js
@@ -0,0 +1,13 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+//A traves de un obejto definir todas las propiedades que queremos tenr 
+const mySchema = new Schema({
+    users: [{
+        type: Schema.ObjectId,
+        ref: 'User',
+    }]
+});
+
+const model = mongoose.model('Chat', mySchema); //Collecion en mongo y el esquema
+module.exports = model;
