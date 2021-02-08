211e9c9743c6589bb871a2378e3e9a7987563262
German <gnrg_74@hotmail.com>
2021-02-08 10:50:58 -0600

    se creo componente chat

--- network/routes.js
+++ network/routes.js
@@ -2,8 +2,10 @@ express = require('express');
 //Para usar una ruta ya definida en los metodos
 const message = require('../components/message/network');
 const user = require('../components/user/network');
+const chat = require('../components/chat/network');
 const routes = function(server){
     server.use('/message',message);
     server.use('/user',user);
+    server.use('/chat',chat);
 }
 module.exports = routes;
